ecfad2f0069c218ab9233e81a03ca97710a0d603
JinWon Lee <dlwlsdnjs456@naver.com>
2020-09-11 14:38:46 +0900

    Solving

--- src/No_1202.java
+++ src/No_1202.java
@@ -1,5 +1,20 @@
 //https://www.acmicpc.net/problem/1202
 
-public class No_1202 {
+import java.io.*;
+import java.util.StringTokenizer;
 
+public class No_1202 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        for(int i=0; i<N; i++){
+            StringTokenizer st2 = new StringTokenizer(br.readLine());
+            
+        }
+
+
+    }
 }
